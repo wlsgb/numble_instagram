72fb6ec2acc45157008e02d19c30ae44e9360ebf
wlsgb94 <wlsgb94@naver.com>
2023-03-27 20:41:23 +0900

    first commit

---
+++ src/main/java/com/instagram/numble_instagram/NumbleInstagramApplication.java
@@ -0,0 +1,13 @@
+package com.instagram.numble_instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbleInstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbleInstagramApplication.class, args);
+	}
+
+}
