8e4d6ef43eab5c0bf3859d2d4d8ca1f633f7ff00
kimjinhyu <wlsgb94@gmail.com>
2023-03-31 21:57:58 +0900

    feat - 리프레시 토큰 엔티티 추가

---
+++ src/main/java/com/instagram/numble_instagram/model/entity/jwt/RefreshTokenEntity.java
@@ -0,0 +1,39 @@
+package com.instagram.numble_instagram.model.entity.jwt;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+@Table(name = "REFRESH_TOKEN", indexes = {
+    @Index(name = "REFRESH_TOKEN_INDEX1", columnList = "KEY_USER_ID"),
+    @Index(name = "REFRESH_TOKEN_INDEX2", columnList = "REFRESH_TOKEN")
+})
+@Comment("리프레시 토큰 테이블")
+public class RefreshTokenEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "REFRESH_TOKEN_ID", nullable = false)
+  private Long refreshTokenId;
+
+  @Column(name = "REFRESH_TOKEN", nullable = false)
+  private String refreshToken;
+
+  @Column(name = "KEY_USER_ID", nullable = false)
+  private String keyUserId;
+
+}
