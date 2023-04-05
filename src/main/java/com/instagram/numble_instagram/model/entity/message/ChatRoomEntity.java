d898cae1ac42cc41c448149ed229737de420152e
kimjinhyu <wlsgb94@gmail.com>
2023-04-05 21:43:09 +0900

    feat - 채팅방 테이블 추가

--- src/main/java/com/instagram/numble_instagram/model/entity/message/ChatRoomEntity.java
+++ src/main/java/com/instagram/numble_instagram/model/entity/message/ChatRoomEntity.java
@@ -1,6 +1,7 @@
 package com.instagram.numble_instagram.model.entity.message;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.CreationTimestamp;
@@ -10,17 +11,14 @@ import org.hibernate.annotations.ResultCheckStyle;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
-import com.instagram.numble_instagram.model.entity.user.UserEntity;
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -30,33 +28,20 @@ import lombok.NoArgsConstructor;
 @Getter
 @DynamicInsert
 @DynamicUpdate
-@SQLDelete(sql = "UPDATE MESSAGE SET DELETED = TRUE WHERE MSG_ID = ?", check = ResultCheckStyle.COUNT)
+@SQLDelete(sql = "UPDATE CHAT_ROOM SET DELETED = TRUE WHERE MSG_ID = ?", check = ResultCheckStyle.COUNT)
 @Where(clause = "DELETED = FALSE")
 @Entity
-@Table(name = "MESSAGE", indexes = {
-	@Index(name = "MESSAGE_INDEX1", columnList = "DELETED"),
+@Table(name = "CHAT_ROOM", indexes = {
+	@Index(name = "CHAT_ROOM_INDEX1", columnList = "DELETED"),
 })
-@Comment("메세지 테이블")
-public class MessageEntity {
+@Comment("채팅방 테이블")
+public class ChatRoomEntity {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "MSG_ID")
-	@Comment("메세지 ID")
-	private Long msgId;
-
-	@Column(name = "CONTENT", columnDefinition = "NVARCHAR(1000)")
-	@Comment("메세지 내용")
-	private String content;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "CHAT_ROOM_ID")
-	@Comment("메세지 보낸 채팅방 ID")
-	private ChatRoomEntity chatRoom;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "USER_ID")
-	@Comment("메세지 보낸 유저 ID")
-	private UserEntity sendUser;
+	@Column(name = "CHAT_ROOM_ID")
+	@Comment("채팅방 ID")
+	private Long chatRoomId;
 
 	@CreationTimestamp
 	@Column(name = "REG_DATE", nullable = false)
@@ -66,4 +51,7 @@ public class MessageEntity {
 	@Column(name = "DELETED", nullable = false)
 	@Comment("삭제 여부")
 	private boolean deleted = Boolean.FALSE;
+
+	@OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<MessageEntity> messageList;
 }
