6c9b9be08fc8ddc0ce7113067e89e2e7c5530271
kimjinhyu <wlsgb94@gmail.com>
2023-04-10 17:06:23 +0900

    feat - 파일 유틸 추가

--- src/main/java/com/instagram/numble_instagram/util/file/FileUtil.java
+++ src/main/java/com/instagram/numble_instagram/util/file/FileUtil.java
@@ -1,6 +1,18 @@
 package com.instagram.numble_instagram.util.file;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class FileUtil {
+    /**
+     * 파일 객체 존재 여부
+     */
+    public static boolean existFile(MultipartFile file) {
+        return file != null && file.isEmpty();
+    }
+
+    /**
+     * 확장자 추출
+     */
     public static String extractExtension(String originalFilename) {
         int pos = originalFilename.lastIndexOf(".");
         return originalFilename.substring(pos + 1);
