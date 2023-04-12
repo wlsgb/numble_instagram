a4636ec6bac4a0472dbdd13a5fcde7f757834a7a
kimjinhyu <wlsgb94@gmail.com>
2023-04-12 18:29:47 +0900

    feat - 메세지 전송 유즈케이스 추가

--- src/main/java/com/instagram/numble_instagram/usecase/message/CreateMessageUseCase.java
+++ src/main/java/com/instagram/numble_instagram/usecase/message/CreateMessageUseCase.java
@@ -1,6 +1,7 @@
 package com.instagram.numble_instagram.usecase.message;
 
 import com.instagram.numble_instagram.model.dto.message.request.MessageSendRequest;
+import com.instagram.numble_instagram.model.entity.message.ChatRoom;
 import com.instagram.numble_instagram.model.entity.user.User;
 import com.instagram.numble_instagram.service.message.*;
 import com.instagram.numble_instagram.service.user.UserService;
@@ -8,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,7 +20,7 @@ public class CreateMessageUseCase {
     private final UserService userService;
     private final ChatRoomReadService chatRoomReadService;
     private final ChatRoomWriteService chatRoomWriteService;
-    private final UserChatRoomMappedReadService chatRoomUserReadService;
+    private final UserChatRoomMappedReadService userChatRoomMappedReadService;
     private final MessageReadService messageReadService;
     private final MessageWriteService messageWriteService;
 
@@ -25,5 +28,16 @@ public class CreateMessageUseCase {
     public void execute(Long fromUserId, MessageSendRequest messageSendRequest) {
         User fromUser = userService.getUser(fromUserId);
         User toUser = userService.getUser(messageSendRequest.toUserId());
+
+        List<ChatRoom> fromUserChatRoomList = userChatRoomMappedReadService.getChatRoomList(fromUser);
+        List<ChatRoom> toUserChatRoomList = userChatRoomMappedReadService.getChatRoomList(toUser);
+
+        ChatRoom chatRoom = fromUserChatRoomList.stream()
+                .filter(fromChatRoom -> toUserChatRoomList.stream().anyMatch(toChatRoom -> toChatRoom.equals(fromChatRoom)))
+                .findFirst()
+                .orElse(chatRoomWriteService.register(fromUser));
+
+        messageWriteService.sendMessage(chatRoom, fromUser, messageSendRequest.content());
+        chatRoomWriteService.saveLastMessage(chatRoom.getChatRoomId(), messageSendRequest.content());
     }
 }
